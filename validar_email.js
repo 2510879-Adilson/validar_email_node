ada2d07039c268d7b2a201351751ba87f097eb9c
Adilson Oliveira <2510879@sempre.unifacig.edu.br>
2026-04-20 21:30:35 -0300

    refatoração: alteração do arquivo validar_email.js

--- validar_email.js
+++ validar_email.js
@@ -3,9 +3,4 @@ function validarEmail(email) {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(email);
 }
-//console.log(validarEmail("usuario@exemplo.com"));
-//console.log(validarEmail("usuario@exemplo.com")); // true
-//console.log(validarEmail("email-invalido.com"));   // false
-//console.log(validarEmail("usuario@dominio"));      // false
-
 module.exports = validarEmail;
